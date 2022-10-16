4b6aef10467531ccd561c667e345660bbf3382b3
Nimmi gupta <nimmi.gupta@gmail.com>
2022-10-16 09:18:57 -0400

    terraform demo

--- 02-variables/demo.tf
+++ 02-variables/demo.tf
@@ -23,8 +23,20 @@ output "introduction-op" {
 }
 
 output "introduction-op1" {
-    value = "My name is  ${var.introduction[0]} and living in  ${var.introduction[1]} and house number is  ${var.introduction[2]}}"
+    value = "My name is  ${var.introduction[0]} and living in  ${var.introduction[1]} and house number is  ${var.introduction[2]}"
     
 }
 
+variablr ex-map {
+    default = {
+        Name = "Nimmi"
+        address = "Westford"
+        number = 26
+    }
+}
+
+#How to print the map varibale
+output ex-map {
+    value = "My name is ${var.ex-map["Name"]}"
+}
 
