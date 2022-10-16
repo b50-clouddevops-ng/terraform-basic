62003c484a1bfeb926a4cd9701000e0b105e85c3
Nimmi gupta <nimmi.gupta@gmail.com>
2022-10-16 09:06:32 -0400

    terraform demo

--- 02-variables/demo.tf
+++ 02-variables/demo.tf
@@ -22,9 +22,9 @@ output "introduction-op" {
     value = "My name is ${var.introduction[0]}"
 }
 
-output "introduction-op1" {
+/* output "introduction-op1" {
     #value = "My name is  ${var.introduction[0]} and living in  ${var.introduction[1]} and house number is  ${var.introduction[2]}}"
     
 }
-
+ */
 
