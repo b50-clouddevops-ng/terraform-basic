daa7357b88922444a9e1e19c04703c71d21bc346
Nimmi gupta <nimmi.gupta@gmail.com>
2022-10-17 23:28:37 -0400

    terraform demo

--- ex1/ec2/main.tf
+++ ex1/ec2/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "demo" {
-  ami                    = "ami-0427dccbe06f9c360"
+  ami                    = data.aws_ami.my_ami.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [var.sg]
 
--- ex1/sg/main.tf
+++ ex1/sg/main.tf
@@ -4,10 +4,19 @@ resource "aws_security_group" "allow_ssh" {
   description   = "Allow ssh inbound traffic"
 
   ingress {
+    description = "HTTP from INTERNET"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH from INTERNET"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
@@ -15,6 +24,10 @@ resource "aws_security_group" "allow_ssh" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssg_sg"
   }
 
 }
