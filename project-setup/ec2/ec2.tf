410ed39732124c1fa047f13b758948063954978a
Nimmi gupta <nimmi.gupta@gmail.com>
2022-10-18 20:17:40 -0400

    terraform demo

--- project-setup/ec2/ec2.tf
+++ project-setup/ec2/ec2.tf
@@ -1,12 +1,22 @@
-resource "aws_spot_instance_request" "spot_worker" {
+/* resource "aws_spot_instance_request" "spot_worker" {
   ami                    = data.aws_ami.my_ami.id
   instance_type          = "t2.micro"
   wait_for_fulfillment      = true 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
-    Name = "var.COMPONENT"
+    Name = "vars.COMPONENT"
+  } */
+
+resource "aws_instance" "demo" {
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "EC2 instance tf remote state"
   }
+}
 
   /* output "spot_public_ip" {
     value = aws_spot_instance_request.spot_worker.public_ip
--- project-setup/main.tf
+++ project-setup/main.tf
@@ -1,9 +1,7 @@
 module "ec2" {
     source       = "./ec2"
-    COMPONENT    = "frontend"
-    APP_VERSION  = "0.0.2"
 }
 
 output frontend_op {
-    value = module.ec2.spot_public_ip
+    value = module.ec2.
 }
