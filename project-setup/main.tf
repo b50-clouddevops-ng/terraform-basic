0f6531f24b1f8761b09ed496d1f1b4fcdda89d9e
Nimmi gupta <nimmi.gupta@gmail.com>
2022-10-18 20:09:16 -0400

    terraform demo

--- project-setup/main.tf
+++ project-setup/main.tf
@@ -4,7 +4,6 @@ module "frontend" {
     APP_VERSION  = "0.0.2"
 }
 
-/* output frontend_op {
-    value = module.frontend.spot_public_ip
+output frontend_op {
+    value = module.ec2.spot_public_ip
 }
- */
