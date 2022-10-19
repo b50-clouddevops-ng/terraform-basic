35234526f7ad8be76492f4a007d6725da3209365
Nimmi gupta <nimmi.gupta@gmail.com>
2022-10-18 21:17:13 -0400

    terraform demo

--- project-setup/ec2/ec2.tf
+++ project-setup/ec2/ec2.tf
@@ -12,7 +12,7 @@
 resource "aws_instance" "demo" {
   ami                    = data.aws_ami.my_ami.id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh1.id]
 
   tags = {
     Name = var.COMPONENT
